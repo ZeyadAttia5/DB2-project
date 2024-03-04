1de0294a9b4cccf5031c3dc5090aba4130dbde09
Zeyad Mohamed Attia <85710632+ZeyadAttia5@users.noreply.github.com>
2024-03-04 22:14:07 +0200

    added project structure and FileReader.java

---
+++ src/main/java/Page.java
@@ -0,0 +1,26 @@
+
+
+
+import java.io.Serializable;
+import java.util.Vector;
+
+public class Page implements Serializable {
+//    private Vector<Tuple> tuples;
+    private static final String configFilePath = "src/main/resources/DBApp.config";
+    private int maximumRowsCountinPage;
+    private int numberOfPages = 0;
+
+    private FileReader configFileReader;
+
+    public Page() {
+        this.configFileReader = new FileReader(configFilePath);
+        this.maximumRowsCountinPage = Integer.parseInt(configFileReader.getProperty("MaximumRowsCountinPage"));
+        System.out.println("Maximum number of rows per page: "+maximumRowsCountinPage);
+        this.numberOfPages++;
+    }
+
+
+
+
+
+}
