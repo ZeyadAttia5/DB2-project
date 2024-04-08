6b8ba612e7d98c4806f8fb3a77d251102b4df5ac
nabilasherif <nabila.sherif81@gmail.com>
2024-04-08 14:16:50 +0200

    select table no index

--- src/main/java/DBApp.java
+++ src/main/java/DBApp.java
@@ -1,6 +1,7 @@
 
-/** * @author Wael Abouelsaadat */ 
+/** * @author Wael Abouelsaadat */
 
+import java.io.IOException;
 import java.util.Iterator;
 import java.util.Hashtable;
 
@@ -10,18 +11,18 @@ public class DBApp {
 
 
 	public DBApp( ){
-		
+
 	}
 
 
 	//
 
-	// this does whatever initialization you would like 
-	// or leave it empty if there is no code you want to 
-	// execute at application startup 
+	// this does whatever initialization you would like
+	// or leave it empty if there is no code you want to
+	// execute at application startup
 	public void init( ){
-		
-		
+
+
 	}
 
 
@@ -29,135 +30,179 @@ public class DBApp {
 	// strClusteringKeyColumn is the name of the column that will be the primary
 	// key and the clustering column as well. The data type of that column will
 	// be passed in htblColNameType
-	// htblColNameValue will have the column name as key and the data 
+	// htblColNameValue will have the column name as key and the data
 	// type as value
-	public void createTable(String strTableName, 
-							String strClusteringKeyColumn,  
-							Hashtable<String,String> htblColNameType) throws DBAppException{
+	public void createTable(String strTableName, String strClusteringKeyColumn, Hashtable<String,String> htblColNameType) throws DBAppException{
 
-	try {
-		//create the metaData.csv file using the hashtable input and store it in the metaData package
-		csvConverter.convert(htblColNameType, strTableName);
-		//initialize a new table object
-		Table newTable = new Table(strTableName);
-	}
-	catch (Exception e){
-		e.printStackTrace();
-	}
-}
+		try {
 
-	// following method creates a B+tree index 
-	public void createIndex(String   strTableName,
-							String   strColName,
-							String   strIndexName) throws DBAppException{
-		
-		throw new DBAppException("not implemented yet");
+			//initialize a new table object
+			Table newTable = new Table(strTableName);
+			// should do something here to prevent calling createTable twice onthe same parameters from overwriting a serialized object
+			newTable.serialize();
+			//create the metaData.csv file using the hashtable input and store it in the metaData package
+			csvConverter.convert(htblColNameType, strTableName, strClusteringKeyColumn);
+
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 
+	// following method creates a B+tree index
+	public void createIndex(String   strTableName, String   strColName, String   strIndexName) throws DBAppException{
 
-	// following method inserts one row only. 
+		// adjusting metadata file with new index
+		boolean found = csvConverter.addIndexToCSV(strTableName, strColName, strIndexName);
+		if(!found) {
+			DBAppException e = new DBAppException("The index name " + strIndexName + " already exists in " + strTableName );
+			throw e;
+		}
+
+		// retrieving data type for desired column
+		String tmp = csvConverter.getDataType(strTableName, strColName);
+
+		// initialising b+tree
+		BPTree tree = null;
+		if (tmp.equalsIgnoreCase("java.lang.Integer")) {
+			tree = new BPTree<Integer>(10);
+
+		} else if (tmp.equalsIgnoreCase("java.lang.String")) {
+			tree = new BPTree<String>(10);
+
+		} else if (tmp.equalsIgnoreCase("java.lang.Double")) {
+			tree = new BPTree<Double>(10);
+		} else {
+			DBAppException e = new DBAppException("Not found");
+			throw e;
+		}
+
+		tree.serialize(strTableName, strIndexName );
+
+	}
+
+
+	// following method inserts one row only.
 	// htblColNameValue must include a value for the primary key
-	public void insertIntoTable(String strTableName, 
-								Hashtable<String,Object>  htblColNameValue) throws DBAppException{
-	
-		throw new DBAppException("not implemented yet");
+	public void insertIntoTable(String strTableName,
+								Hashtable<String,Object>  htblColNameValue) throws DBAppException, IOException, ClassNotFoundException {
+
+		Table target = Table.deserialize("src/main/resources/tables/"+strTableName+"/"+strTableName+".class");
+		Tuple newTuple = new Tuple(htblColNameValue);
+		target.insert(newTuple);
+		System.out.println(Page.deserialize(target.tablePages.get(0)));
 	}
 
 
 	// following method updates one row only
-	// htblColNameValue holds the key and new value 
+	// htblColNameValue holds the key and new value
 	// htblColNameValue will not include clustering key as column name
 	// strClusteringKeyValue is the value to look for to find the row to update.
-	public void updateTable(String strTableName, 
+	public void updateTable(String strTableName,
 							String strClusteringKeyValue,
 							Hashtable<String,Object> htblColNameValue   )  throws DBAppException{
-	
+
 		throw new DBAppException("not implemented yet");
 	}
 
 
 	// following method could be used to delete one or more rows.
-	// htblColNameValue holds the key and value. This will be used in search 
-	// to identify which rows/tuples to delete. 	
+	// htblColNameValue holds the key and value. This will be used in search
+	// to identify which rows/tuples to delete.
 	// htblColNameValue enteries are ANDED together
-	public void deleteFromTable(String strTableName, 
+	public void deleteFromTable(String strTableName,
 								Hashtable<String,Object> htblColNameValue) throws DBAppException{
-	
+
 		throw new DBAppException("not implemented yet");
 	}
 
 
-	public Iterator selectFromTable(SQLTerm[] arrSQLTerms, 
+	public Iterator selectFromTable(SQLTerm[] arrSQLTerms,
 									String[]  strarrOperators) throws DBAppException{
-										
+
 		return null;
 	}
 
 
 	public static void main( String[] args ){
-	
-	try{
+
+		try{
+
 			String strTableName = "Student";
 			DBApp	dbApp = new DBApp( );
-			
+//
 			Hashtable htblColNameType = new Hashtable( );
 			htblColNameType.put("id", "java.lang.Integer");
 			htblColNameType.put("name", "java.lang.String");
 			htblColNameType.put("gpa", "java.lang.double");
 			dbApp.createTable( strTableName, "id", htblColNameType );
+
+			String strTableName2 = "Girl";
+
+			Hashtable htblColNameType2 = new Hashtable<>();
+			htblColNameType2.put("id", "java.lang.Integer");
+			htblColNameType2.put("name", "java.lang.String");
+			htblColNameType2.put("gpa", "java.lang.Integer");
+			dbApp.createTable( strTableName2, "gpa", htblColNameType2 );
+
 			dbApp.createIndex( strTableName, "gpa", "gpaIndex" );
 
-			Hashtable htblColNameValue = new Hashtable( );
-			htblColNameValue.put("id", new Integer( 2343432 ));
-			htblColNameValue.put("name", new String("Ahmed Noor" ) );
-			htblColNameValue.put("gpa", new Double( 0.95 ) );
-			dbApp.insertIntoTable( strTableName , htblColNameValue );
+			BPTree b = BPTree.deserialize("Student", "gpa");
 
-			htblColNameValue.clear( );
-			htblColNameValue.put("id", new Integer( 453455 ));
-			htblColNameValue.put("name", new String("Ahmed Noor" ) );
-			htblColNameValue.put("gpa", new Double( 0.95 ) );
-			dbApp.insertIntoTable( strTableName , htblColNameValue );
-
-			htblColNameValue.clear( );
-			htblColNameValue.put("id", new Integer( 5674567 ));
-			htblColNameValue.put("name", new String("Dalia Noor" ) );
-			htblColNameValue.put("gpa", new Double( 1.25 ) );
-			dbApp.insertIntoTable( strTableName , htblColNameValue );
-
-			htblColNameValue.clear( );
-			htblColNameValue.put("id", new Integer( 23498 ));
-			htblColNameValue.put("name", new String("John Noor" ) );
-			htblColNameValue.put("gpa", new Double( 1.5 ) );
-			dbApp.insertIntoTable( strTableName , htblColNameValue );
-
-			htblColNameValue.clear( );
-			htblColNameValue.put("id", new Integer( 78452 ));
-			htblColNameValue.put("name", new String("Zaky Noor" ) );
-			htblColNameValue.put("gpa", new Double( 0.88 ) );
-			dbApp.insertIntoTable( strTableName , htblColNameValue );
-
-
-			SQLTerm[] arrSQLTerms;
-			arrSQLTerms = new SQLTerm[2];
-			arrSQLTerms[0]._strTableName =  "Student";
-			arrSQLTerms[0]._strColumnName=  "name";
-			arrSQLTerms[0]._strOperator  =  "=";
-			arrSQLTerms[0]._objValue     =  "John Noor";
-
-			arrSQLTerms[1]._strTableName =  "Student";
-			arrSQLTerms[1]._strColumnName=  "gpa";
-			arrSQLTerms[1]._strOperator  =  "=";
-			arrSQLTerms[1]._objValue     =  new Double( 1.5 );
-
-			String[]strarrOperators = new String[1];
-			strarrOperators[0] = "OR";
-			// select * from Student where name = "John Noor" or gpa = 1.5;
-			Iterator resultSet = dbApp.selectFromTable(arrSQLTerms , strarrOperators);
+//			dbApp.createIndex( strTableName, "gpa", "gpaIndex" );
+//
+//			Hashtable htblColNameValue = new Hashtable( );
+//			htblColNameValue.put("id", new Integer( 0 ));
+//			htblColNameValue.put("name", new String("Ahmed Noor" ) );
+//			htblColNameValue.put("gpa", new Double( 0.95 ) );
+//			dbApp.insertIntoTable( strTableName , htblColNameValue );
+//
+//			htblColNameValue.clear( );
+//			htblColNameValue.put("id", new Integer( 453455 ));
+//			htblColNameValue.put("name", new String("Ahmed Noor" ) );
+//			htblColNameValue.put("gpa", new Double( 0.95 ) );
+//			dbApp.insertIntoTable( strTableName , htblColNameValue );
+//
+//			htblColNameValue.clear( );
+//			htblColNameValue.put("id", new Integer( 5674567 ));
+//			htblColNameValue.put("name", new String("Dalia Noor" ) );
+//			htblColNameValue.put("gpa", new Double( 1.25 ) );
+//			dbApp.insertIntoTable( strTableName , htblColNameValue );
+//
+//			htblColNameValue.clear( );
+//			htblColNameValue.put("id", new Integer( 23498 ));
+//			htblColNameValue.put("name", new String("John Noor" ) );
+//			htblColNameValue.put("gpa", new Double( 1.5 ) );
+//			dbApp.insertIntoTable( strTableName , htblColNameValue );
+//
+//			htblColNameValue.clear( );
+//			htblColNameValue.put("id", new Integer( 78452 ));
+//			htblColNameValue.put("name", new String("Zaky Noor" ) );
+//			htblColNameValue.put("gpa", new Double( 0.88 ) );
+//			dbApp.insertIntoTable( strTableName , htblColNameValue );
+//
+//
+//			SQLTerm[] arrSQLTerms;
+//			arrSQLTerms = new SQLTerm[2];
+//			arrSQLTerms[0]._strTableName =  "Student";
+//			arrSQLTerms[0]._strColumnName=  "name";
+//			arrSQLTerms[0]._strOperator  =  "=";
+//			arrSQLTerms[0]._objValue     =  "John Noor";
+//
+//			arrSQLTerms[1]._strTableName =  "Student";
+//			arrSQLTerms[1]._strColumnName=  "gpa";
+//			arrSQLTerms[1]._strOperator  =  "=";
+//			arrSQLTerms[1]._objValue     =  new Double( 1.5 );
+//
+//			String[]strarrOperators = new String[1];
+//			strarrOperators[0] = "OR";
+//			// select * from Student where name = "John Noor" or gpa = 1.5;
+//			Iterator resultSet = dbApp.selectFromTable(arrSQLTerms , strarrOperators);
 		}
 		catch(Exception exp){
 			exp.printStackTrace( );
 		}
 	}
+
 
 }
--- src/main/java/csvConverter.java
+++ src/main/java/csvConverter.java
@@ -1,12 +1,33 @@
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.Enumeration;
-import java.util.Hashtable;
+import java.io.*;
+import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.*;
+
 public class csvConverter {
-    public static void convert(Hashtable<String, String> hashtable, String tableName) {
-        try (FileWriter writer = new FileWriter("metaData/" + tableName + ".csv")) {
-            // Write header to CSV file
-            writer.append("Table Name, Column Name, Column Type, ClusteringKey, IndexName, IndexType\n");
+
+    private static final String METADATA_FILE = "src/metadata.csv";
+
+    public static void convert(Hashtable<String, String> hashtable, String tableName, String strClusteringKeyColumn) {
+        try (FileReader fileReader = new FileReader(METADATA_FILE);
+             BufferedReader bufferedReader = new BufferedReader(fileReader);
+             FileWriter writer = new FileWriter(METADATA_FILE, true)) {
+
+            // Check if the table name already exists in metadata
+            String line;
+            while ((line = bufferedReader.readLine()) != null) {
+                String[] parts = line.split(",");
+                String existingTableName = parts[0].trim();
+                if (existingTableName.equals(tableName)) {
+                    System.out.println("Table name '" + tableName + "' already exists in the CSV file. Cannot convert.");
+                    return;
+                }
+            }
+
+            // Write header to CSV file if it's empty
+            if (Files.size(Path.of(METADATA_FILE)) == 0) {
+                writer.write("Table Name, Column Name, Column Type, ClusteringKey, IndexName, IndexType\n");
+            }
 
             // Iterate over the hashtable entries
             Enumeration<String> keys = hashtable.keys();
@@ -19,26 +40,140 @@ public class csvConverter {
                     columnType = "null";
                 }
 
+                // Setting cluster key
+                String isCluster = "False";
+                if (columnName.equals(strClusteringKeyColumn)) {
+                    isCluster = "True";
+                }
+
                 // Write hashtable entry to CSV file
                 writer.append(tableName).append(",") // Table Name
                         .append(columnName).append(",") // Column Name
                         .append(columnType).append(",") // Column Type
-                        .append("null").append(",") // ClusteringKey
+                        .append(isCluster).append(",") // ClusteringKey
                         .append("null").append(",") // IndexName
                         .append("null"); // IndexType
                 writer.append("\n");
             }
 
-            System.out.println("CSV file successfully created at: " + "metaData/" + tableName + ".csv");
+            System.out.println("Table metadata successfully added.");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
-//    public static void main(String[] args){
+
+    public static String getDataType(String strTableName, String strColName){
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(METADATA_FILE))) {
+            String line;
+            while ((line = reader.readLine()) != null ) {
+                String[] fields = line.split(",");
+                if (fields[0].equals(strTableName) && fields[1].equals(strColName))
+                    return fields[2];
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+
+    }
+
+    public static String getIndexName(String tableName, String colName){
+        try (BufferedReader reader = new BufferedReader(new FileReader(METADATA_FILE))) {
+            String line;
+            while ((line = reader.readLine()) != null ) {
+                String[] fields = line.split(",");
+                if (fields[0].equals(tableName) && fields[1].equals(colName))
+                    return fields[4];
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static String getClusteringKey(String tableName) {
+        try (BufferedReader reader = new BufferedReader(new FileReader(METADATA_FILE))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length >= 4 && parts[0].equalsIgnoreCase(tableName) && parts[3].equalsIgnoreCase("True")) {
+                    return parts[1]; // Return the clustering key column name
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null; // No clustering key found for the table
+    }
+
+
+
+    // Adjusting the metadata file
+    public static boolean addIndexToCSV(String strTableName, String strColName, String strIndexName) {
+
+        // Getting names of all indices for table of interest
+        HashSet<String> existingIndex = new HashSet<>();
+        try (BufferedReader reader1 = new BufferedReader(new FileReader(METADATA_FILE))) {
+            String line;
+            while ((line = reader1.readLine()) != null ) {
+                String[] fields = line.split(",");
+                if (fields[0].equals(strTableName) )
+                    existingIndex.add(fields[4]);
+                if(!fields[0].equals(strTableName) && !existingIndex.isEmpty())
+                    break;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        // Updating csv file
+        List<String> lines = new ArrayList<>();
+        boolean found = false;
+        try (BufferedReader reader = new BufferedReader(new FileReader(METADATA_FILE))) {
+            String line;
+
+            while ((line = reader.readLine()) != null  ) {
+                String[] fields = line.split(",");
+                if (fields[0].equals(strTableName) && fields[1].equals(strColName) && !existingIndex.contains(strIndexName)) {
+                    found = true;
+                    fields[4] = strIndexName;
+                    fields[5] = "B+Tree";
+                    line = String.join(",", fields);
+                }
+
+                lines.add(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(METADATA_FILE))) {
+            for (String modifiedLine : lines) {
+                writer.write(modifiedLine);
+                writer.newLine();
+            }
+
+            System.out.println(found ? "Index added." : "This index name already exists in this table.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return found;
+    }
+
+    public static void main(String[] args){
 //        Hashtable htblColNameType = new Hashtable( );
 //        htblColNameType.put("id", "java.lang.Integer");
 //        htblColNameType.put("name", "java.lang.String");
 //        htblColNameType.put("gpa", "java.lang.double");
-//        convert(htblColNameType,"test");
-//    }
-} 
+//        convert(htblColNameType,"test", "id");
+
+        // testing with same table name and same index name
+        addIndexToCSV("Girl","gpa","GpaIndex");// should work
+        addIndexToCSV("Girl","id","IdIndex");// should not work bec same name
+
+
+    }
+}
