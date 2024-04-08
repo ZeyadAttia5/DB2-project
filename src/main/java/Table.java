447b115a52a99eedbc68f82082b269b3c4dfd11d
nabilasherif <nabila.sherif81@gmail.com>
2024-04-08 13:40:34 +0200

    select table no index

--- src/main/java/Table.java
+++ src/main/java/Table.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 
+
 public class Table implements Serializable {
     // Map to store the number of pages for each table
     public Vector<String> tablePages;
@@ -18,58 +19,60 @@ public class Table implements Serializable {
 
     public static void createDirectory(String folderPath) {
         // Create a File object representing the directory
-        File directory = new File(folderPath);
+        File directory = new File("src/main/resources/tables/" + folderPath);
 
         // Create the directory if it doesn't exist
         if (!directory.exists()) {
             boolean created = directory.mkdirs();
             if (created) {
-                System.out.println("Directory created successfully: " + folderPath);
+                System.out.println("Directory created successfully: " + "src/main/resources/tables/" + folderPath);
             } else {
-                System.out.println("Failed to create directory: " + folderPath);
+                System.out.println("Failed to create directory: " + "src/main/resources/tables/" + folderPath);
             }
         } else {
-            System.out.println("Directory already exists: " + folderPath);
+            System.out.println("Directory already exists: " + "src/main/resources/tables/" + folderPath);
         }
     }
 
 
     // Method to get page count for a table
     public int getPageCount() {
-       return tablePages.size();
+        return tablePages.size();
     }
-    public void insert(Tuple tuple) throws DBAppException {
-         if (getPageCount() == 0) {
-            Page page = new Page(name,this.tablePages.size());
-            page.insert(tuple);
-            tablePages.add(page.name);
-        } else {
-            Vector<String> pageFileNames = this.tablePages;
-            int lastIndex = pageFileNames.size() - 1;
-            int currentIndex = 0;
-            for (String fileName : pageFileNames) {//still not handled when full
-                Page page = Page.deserialize(name + "/" + fileName + ".class");
-                if(!page.isFull()) {
-                    page.insert(tuple);
-                    break;
-                }
-                else if (currentIndex == lastIndex){
-                    Page nPage = new Page(name, this.tablePages.size());
-                    nPage.insert(tuple);
-                }
-                currentIndex++;
-            }
 
-
+    public void insert(Tuple tuple) throws DBAppException, IOException, ClassNotFoundException {
+        if (this.tablePages.size()==0)
+        {
+            Page newPage = new Page(this.name,this.tablePages.size(),csvConverter.getClusteringKey(this.name));
+            newPage.insert(tuple);
+            tablePages.add(newPage.name);
+            this.serialize();
+            return;
         }
+        String clusteringKey = csvConverter.getClusteringKey(this.name);
+        Object targetKey = tuple.values.get(clusteringKey);
+        Page currPage = null;
+        int result=1;
+        for(int i = 0; result > 0; i++)
+        {
+            currPage = Page.deserialize(this.name+"_"+i);
+
+            result =  ((Comparable) targetKey).compareTo((Comparable) currPage.max);
+        }
+        currPage.insert(tuple);
+        this.serialize();
+
+
+
+
     }
 
     public void serialize() {
         String tableName = name;
-        try (FileOutputStream fos = new FileOutputStream(tableName + "/" + name + ".class" );
+        try (FileOutputStream fos = new FileOutputStream("src/main/resources/tables/" + tableName + "/" + name + ".class" );
              ObjectOutputStream out = new ObjectOutputStream(fos)) {
-                out.writeObject(this);
-                System.out.println("saved table successfully at " + tableName + "/" + name + ".class" );
+            out.writeObject(this);
+            System.out.println("saved table successfully at " + "src/main/resources/tables/" + tableName + "/" + name + ".class" );
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -88,7 +91,7 @@ public class Table implements Serializable {
     }
 
     // Method to print all serialized pages for the specified table name
-    public static void printTable(String tableName) {
+    public static void printTable(String tableName) throws IOException, ClassNotFoundException {
         File directory = new File(tableName); // Use provided table name as directory name
         FilenameFilter filter = (dir, name) -> name.endsWith(".class");
         File[] serializedPages = directory.listFiles(filter);
@@ -105,7 +108,127 @@ public class Table implements Serializable {
             System.out.println("No serialized pages found for table " + tableName);
         }
     }
+    public ArrayList<Tuple> searchTable(String columnName, String operator, Object value) throws DBAppException {
+        ArrayList<Tuple> results = new ArrayList<>();
+        Vector<String[]> columnstuff = Page.readCSV(this.name);
+        ArrayList<Tuple> pageResults = new ArrayList<Tuple>();
+        String isclusteringkey = "False";
+        String columnType=null;
+        for (String[] column : columnstuff) {
+            if (column[1].equals(columnName)) {
+                isclusteringkey = column[3];
+                columnType = column[2];
+                break;
+            }
+        }
+        if (columnType == null) {
+            throw new DBAppException("Column "+ columnName +" not found");
+        }
+        if (!compatibleTypes(value, columnType)) {
+            throw new DBAppException("Datatype of value doesn't match the column datatype: ");
+        }
+        if (isclusteringkey == "False" || operator == "!=") {
+            for (String pagename : tablePages) {
+                try {
+                    Page page = Page.deserialize(pagename + ".class");// you removed hagat mn hena gt mn salma's incase 3amal moshkela
+                    pageResults = new ArrayList<Tuple>();
+                    pageResults = page.searchlinearPage(columnName, value, operator);
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                }
+                results.addAll(pageResults);
+            }
+            return results;
+        } else {
+            if(operator.equals("=")){
+                Comparable<Object> comparableValue = (Comparable<Object>) value;
+                int low = 0;
+                int high = tablePages.size() - 1;
+                while (low <= high) {
+                    int mid = (low + high) / 2;
+                    String pagename = tablePages.get(mid);
+                    try {//getters and setterssss!
+                        Page page = Page.deserialize(pagename + ".class");
+                        if (comparableValue.compareTo(page.min) < 0) {
+                            high = mid - 1; // Search in the lower half
+                        } else if (comparableValue.compareTo(page.max) > 0) {
+                            low = mid + 1; // Search in the upper half
+                        } else {
+                            pageResults = page.binarysearchPage(columnName, value, operator);
+                            results.addAll(pageResults);
+                            return results;
+                        }
+                    } catch (IOException | ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            if(operator==">"||operator==">=") {
+                for (int i = tablePages.size() - 1; i >= 0; i--) {
+                    try {
+                        Page page = Page.deserialize(this.tablePages.get(i)+".class");
+                        Object maximum =page.max;
+                        if (((Comparable) maximum).compareTo((Comparable) value) > 0) {
+                            ArrayList<Tuple> tempp = new ArrayList<>();
+                            pageResults=new ArrayList<>();
+                            tempp = page.binarysearchPage(columnName, value, operator);
+                            page.serialize();
+                            for(int i= tempp.size()-1;i>=0;--){
+                                pageResults.add(tempp.remove(i));
+                            }
+                            results.addAll(pageResults);
+                        }else{
+                            return results;
+                        }
+                    } catch (IOException | ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            if(operator==">"||operator==">=") {
+                for (int i = tablePages.size() - 1; i >= 0; i--) {
+                    try {
+                        Page page = Page.deserialize(this.tablePages.get(i)+".class");
+                        int maximum =page.max;
+                        if (((Comparable) maximum).compareTo((Comparable) value) > 0) {
+                            ArrayList<Tuple> tempp = new ArrayList<>();
+                            tempp = page.binarysearchPage(columnName, value, operator);
+                            page.serialize();
+                            pageResults.addAll(temp);
+                        }else{
+                            for(int i= pageResults.size()-1;i>=0;--){
+                                results.add(pageResults.get(i));
+                            }
+                            return results;
+                        }
+                    } catch (IOException | ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            if (operator=="<"||operator=="<=") {
+                for (int i=0;i< tablePages.size();i++){
+                    try {
+                        Page page = Page.deserialize(this.tablePages.get(i)+".class");
+                        Object minimum =page.min;
+                        if (((Comparable) minimum).compareTo((Comparable) value) < 0) {
+                            pageResults = new ArrayList<Tuple>();
+                            pageResults = page.binarysearchPage(columnName, value, operator);
+                            page.serialize();
 
+                            results.addAll(pageResults);
+                        }
+                        else{
+                            return results;
+                        }
+                    } catch (IOException | ClassNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+        return results;
+    }
     public static void main(String[] args){
         Table t1 = new Table("Student");
         t1.serialize();
